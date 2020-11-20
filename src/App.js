caad7aaca1d01d16bbdc1f95d6917a7cdc1c2959
Moses <moses@resetmsm.com>
2020-11-20 21:47:25 +0000

    Completed the UI

--- src/App.js
+++ src/App.js
@@ -31,6 +31,49 @@ function App() {
               </div>
             </div>
           </div>
+          <div className="form-group second">
+            <div className="flex-container">
+              <div className="flex-item heading">Step 2: More comments</div>
+              <div className="flex-item top">
+                <div className="input-group">
+                  <label htmlFor="number"> Mobile Number</label>
+                  <input type="text" name="number" id="number" onChange={handleChange} />
+                </div>
+                <div className="input-group">
+                  <label htmlFor="gender"> Gender </label>
+                  <input type="text" name="gender" id="gender" onChange={handleChange} />
+                </div>
+                <div className="input-group">
+                  <label htmlFor="dob"> Date of Birth</label>
+                  <input type="text" name="dob" id="dob" onChange={handleChange} />
+                </div>
+              </div>
+              <div className="flex-item bottom">
+                <button className="btn" type="submit">
+                  <i className="fas fa-chevron-left"></i> Prev
+                </button>
+                <button className="btn" type="submit">
+                  Next <i className="fas fa-chevron-right"></i>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="form-group">
+            <div className="flex-container">
+              <div className="flex-item heading">Step 3: Final Comments</div>
+              <div className="flex-item top">
+                <div className="input-group">
+                  <label htmlFor="comments"> Comments </label>
+                  <textarea name="comments" id="comments" onChange={handleChange} required></textarea>
+                </div>
+              </div>
+              <div className="flex-item bottom">
+                <button className="btn" type="submit">
+                  Submit <i className="fas fa-chevron-right"></i>
+                </button>
+              </div>
+            </div>
+          </div>
         </form>
       </div>
     </main>
