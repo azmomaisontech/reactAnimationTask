e21a5a85d2894afa2de75e219e108ffc70910d12
Moses <moses@resetmsm.com>
2020-11-20 21:32:18 +0000

    First form prototype

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
 import "./App.css";
 
 function App() {
-  return <h1>Hello World</h1>;
+  const handleChange = (e) => {};
+
+  return (
+    <main>
+      <div className="container">
+        <form>
+          <div className="form-group">
+            <div className="flex-container">
+              <div className="flex-item heading">Step 1: Your details</div>
+              <div className="flex-item top">
+                <div className="input-group">
+                  <label htmlFor="firstname"> First Name</label>
+                  <input type="text" name="firstname" id="firstname" onChange={handleChange} />
+                </div>
+                <div className="input-group">
+                  <label htmlFor="surname"> Surname </label>
+                  <input type="text" name="surname" id="surname" onChange={handleChange} />
+                </div>
+                <div className="input-group">
+                  <label htmlFor="email"> Email Address:</label>
+                  <input type="email" name="email" id="email" onChange={handleChange} />
+                </div>
+              </div>
+              <div className="flex-item bottom">
+                <button className="btn" type="submit">
+                  Next <i className="fas fa-chevron-right"></i>
+                </button>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </main>
+  );
 }
 
 export default App;
